8c007177cd4fbf78344866e1326b95d97f5e2a25
joseppbenvenuto <50219181+joseppbenvenuto@users.noreply.github.com>
2022-01-19 16:12:25 -0500

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -7,7 +7,6 @@ def create_database():
     - Creates and connects to the sparkifydb
     - Returns the connection and cursor to sparkifydb
     """
-
     # connect to default database
     conn = ps.connect('''host=localhost 
                          dbname=studentdb 
@@ -37,7 +36,7 @@ def create_database():
 
 def drop_tables(cur, conn):
     """
-    Drops each table using the queries in `drop_table_queries` list.
+    - Drops each table using the queries in `drop_table_queries` list
     """
     for query in drop_table_queries:
         cur.execute(query)
@@ -46,7 +45,7 @@ def drop_tables(cur, conn):
 
 def create_tables(cur, conn):
     """
-    Creates each table using the queries in `create_table_queries` list.
+    - Creates each table using the queries in `create_table_queries` list
     """
     for query in create_table_queries:
         cur.execute(query)
@@ -55,16 +54,12 @@ def create_tables(cur, conn):
 
 def main():
     """
-    - Drops (if exists) and Creates the sparkify database.
-
+    - Drops (if exists) and Creates the sparkify database
     - Establishes connection with the sparkify database and gets
-    cursor to it.
-
-    - Drops all the tables.
-
-    - Creates all tables needed.
-
-    - Finally, closes the connection.
+      cursor to it.
+    - Drops all the tables
+    - Creates all tables needed
+    - Finally, closes the connection
     """
     cur, conn = create_database()
 
